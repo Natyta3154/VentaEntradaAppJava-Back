a14a550582cc2342cb62f460dfca86be15b12d27
hernan3154 <herny3154@gmail.com>
2026-09-13 05:25:54 -0300

    Use RENAME COLUMN in V10 for TiDB

--- src/main/resources/db/migration/V10__update_entradas_auditoria.sql
+++ src/main/resources/db/migration/V10__update_entradas_auditoria.sql
@@ -1,5 +1,5 @@
 -- V10__update_entradas_auditoria.sql
-ALTER TABLE entradas CHANGE COLUMN qr_code codigo_qr VARCHAR(255) NOT NULL UNIQUE;
-ALTER TABLE entradas CHANGE COLUMN fecha_uso fecha_ingreso TIMESTAMP NULL;
+ALTER TABLE entradas RENAME COLUMN qr_code TO codigo_qr;
+ALTER TABLE entradas RENAME COLUMN fecha_uso TO fecha_ingreso;
 ALTER TABLE entradas ADD COLUMN validado_por_id BIGINT;
 ALTER TABLE entradas ADD CONSTRAINT fk_entrada_portero FOREIGN KEY (validado_por_id) REFERENCES usuarios(id);
